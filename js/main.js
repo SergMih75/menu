27a0c7d80250d767ac12d1a5d8559f32d916cbb2
SergMih75 <109480381+SergMih75@users.noreply.github.com>
2023-12-28 00:35:44 +0300

    menu crated

---
+++ js/main.js
@@ -0,0 +1,18 @@
+let menuSubitem = document.querySelectorAll('.menu-subitem')
+let menuItem = document.querySelectorAll('.menu-item')
+
+menuSubitem.forEach(menuSubitem => {
+	menuSubitem.addEventListener('click', e => {
+		e.stopPropagation()
+		menuSubitem.style.display = 'none'
+	})
+})
+
+menuItem.forEach(menuItem => {
+	menuItem.addEventListener('click', () => {
+		menuSubitem.forEach(menuSubitem => {
+			menuSubitem.style.display = 'none'
+		})
+		menuItem.childNodes[2].style.display = 'block'
+	})
+})
